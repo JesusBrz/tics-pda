10ae5be74e0f5d2ca2f49d77e8fed0116dfd5e60
Jesús Miguel Bruzón Pimienta <jbruzonpimienta@gmail.com>
2022-09-14 11:50:07 -0500

    remove google cloud config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,6 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.active_storage.service = :google
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
