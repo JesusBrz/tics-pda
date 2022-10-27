a63d2197e21e34c06652ada0ce5e513f88d5795a
Jesús Miguel Bruzón Pimienta <jbruzonpimienta@gmail.com>
2022-10-26 20:41:15 -0500

    out of range indicator

--- app/helpers/printers_helper.rb
+++ app/helpers/printers_helper.rb
@@ -2,4 +2,8 @@ module PrintersHelper
   def has_color(printer)
     return true if printer[2] == "C"
   end
+  def in_range(printer)
+    host_id = printer.split(".")
+    return true if host_id[3].to_i.between?(245, 254)
+  end
 end
