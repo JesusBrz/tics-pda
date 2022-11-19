f084656daba4646677fd511d18bee7f3dc7c1f5e
Jesús Miguel Bruzón Pimienta <jbruzonpimienta@gmail.com>
2022-11-19 15:06:57 -0500

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
+npm install flowbite
 bundle install
-bundle exec ./bin/rails tailwindcss:install
+bundle exec rails tailwindcss:build
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
